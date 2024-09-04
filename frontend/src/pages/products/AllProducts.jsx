7fc45507a50d0816349226f8b81ac959d3e43716
d3sc <vinary686@gmail.com>
2024-09-04 23:20:08 +0700

    add feature: product management

--- frontend/src/pages/products/AllProducts.jsx
+++ frontend/src/pages/products/AllProducts.jsx
@@ -1,18 +1,79 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Check from "../../middleware/auth/Check";
 import { AuthContext } from "../../context/AuthContext";
 import NotFound from "../NotFound";
+import { ApiProducts } from "../../helpers/api";
+import { Link } from "react-router-dom";
 
 export default function AllProducts() {
   const isGuest = Check.isGuest();
   const { user } = useContext(AuthContext);
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    ApiProducts.get().then(({ data }) => setData(data));
+  }, []);
+
   if (isGuest || user?.name != "admin")
     return <NotFound code={401} message="Unauthorized user" />;
-  if (!user) return "Loading..";
+  if (!user || !data) return "Loading..";
   return (
-    <div>
+    <div className="p-4">
       <h1>Products manager page</h1>
-      {user.name}
+      <div className="mt-6">
+        <button className="bg-brandBlue/95 text-white py-2 px-6 rounded-md hover:bg-brandBlue">
+          create
+        </button>
+        <div className="overflow-x-auto">
+          <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
+            <thead className="ltr:text-left rtl:text-right">
+              <tr className="">
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 text-start">
+                  Name
+                </th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 text-start">
+                  Date of Birth
+                </th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 text-start">
+                  Role
+                </th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 text-start">
+                  Salary
+                </th>
+                <th className="px-4 py-2"></th>
+              </tr>
+            </thead>
+
+            <tbody className="divide-y divide-gray-200">
+              {data?.map((item, index) => (
+                <tr key={index}>
+                  <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                    <img
+                      src={item.image}
+                      alt=""
+                      className="w-32 h-32 rounded-md border-4 border-white"
+                    />
+                  </td>
+                  <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                    {item.name}
+                  </td>
+                  <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                    $120,000
+                  </td>
+                  <td className="whitespace-nowrap px-4 py-2">
+                    <Link
+                      to={`/dashboard/product/${item.id}`}
+                      className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
+                    >
+                      Detail
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
